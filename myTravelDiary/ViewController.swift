6ac2911bb974d04f35a26f0e8a75d472fd9fb860
Kazuya Nakazato <magmanakazato@gmail.com>
2020-05-19 23:37:40 +0900

    first commit

--- myTravelDiary/ViewController.swift
+++ myTravelDiary/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import Lottie
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showAnimation()
     }
+    
+    func showAnimation() {
+        let animationView = AnimationView(name: "mapAnimation")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        animationView.center = self.view.center
+        animationView.loopMode = .playOnce
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 1
 
+        view.addSubview(animationView)
+
+        animationView.play { finished in
+            if finished {
+                animationView.removeFromSuperview()
+                let nextVC = MainTabBarViewController()
+                self.addChild(nextVC)
+                self.view.addSubview(nextVC.view!)
+                nextVC.didMove(toParent: self)
+            }
+        }
+    }
 
 }
 
