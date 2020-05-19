78411930e364a9cca3680eda0097cd27fde6ab66
Kazuya Nakazato <magmanakazato@gmail.com>
2020-05-19 22:47:11 +0900

    Initial Commit

---
+++ myTravelDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myTravelDiary
+//
+//  Created by 仲里和也 on 2020/05/19.
+//  Copyright © 2020 仲里和也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
